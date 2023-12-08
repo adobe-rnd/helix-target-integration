6196824be34a6b6067e279f04b7932e878a2d71c
Vitaly Tsaplin <tsaplin@adobe.com>
2023-12-08 10:26:54 +0100

    refactor: code cleanup

--- scripts/target.js
+++ scripts/target.js
@@ -85,9 +85,9 @@ async function fetchOffers(client, host, sessionId) {
   const payload = {
     context: {
       channel: 'web',
-      property: {
-        token: 'dev-helix-target-integration',
-      },
+      // property: {
+      //   token: 'dev-helix-target-integration',
+      // },
       address: {
         url: host,
       },
